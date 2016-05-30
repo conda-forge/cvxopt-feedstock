d9031f7ade543c917b10d7fd703826e9855b361f
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-30 12:39:12 -0400

    recipe: Use our copy of SuiteSparse 4.4.x.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 
-if [ "$(uname)" == "Linux" ];
-then
-    # SuiteSparse needs the Real Time library on Linux.
-    export CFLAGS="${CFLAGS} -lrt"
-fi
-
 export CVXOPT_BLAS_LIB_DIR="${PREFIX}/lib"
 export CVXOPT_BLAS_LIB="openblas"
 export CVXOPT_LAPACK_LIB="openblas"
@@ -23,11 +17,8 @@ export CVXOPT_BUILD_GLPK=1
 export CVXOPT_GLPK_LIB_DIR="${PREFIX}/lib"
 export CVXOPT_GLPK_INC_DIR="${PREFIX}/include"
 
-#
-# Once we build SuiteSparse, we can revisit this.
-#
-#export CVXOPT_SUITESPARSE_EXT_LIB=1
-#export CVXOPT_AMD_EXT_LIB="${PREFIX}/lib"
-#export CVXOPT_AMD_EXT_LIB="${PREFIX}/include"
+export CVXOPT_SUITESPARSE_EXT_LIB=1
+export CVXOPT_AMD_EXT_LIB="${PREFIX}/lib"
+export CVXOPT_AMD_EXT_INC="${PREFIX}/include"
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt

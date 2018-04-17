a344521d11bdef7ba7076b093c75cc44dda9434f
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2018-04-17 10:51:13 +0200

    Update to CVXOPT 1.2.0

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+
+set CVXOPT_MSVC=1
+set "CVXOPT_BLAS_LIB_DIR=%LIBRARY_LIB%"
+set CVXOPT_BLAS_LIB=openblas
+set CVXOPT_LAPACK_LIB=openblas
+
+set CVXOPT_BUILD_GLPK=1
+set "CVXOPT_GLPK_LIB_DIR=%LIBRARY_LIB%"
+set "CVXOPT_GLPK_INC_DIR=%LIBRARY_INC%"
+
+set "CVXOPT_SUITESPARSE_LIB_DIR=%LIBRARY_LIB%"
+set "CVXOPT_SUITESPARSE_INC_DIR=%LIBRARY_INC%\suitesparse"
+
+%PYTHON% setup.py install --single-version-externally-managed --record=record.txt

201141fbaa1b783d05e056c47ae6442887d13134
Michael Sarahan <msarahan@gmail.com>
2018-06-12 16:04:53 -0500

    update; add mkl; add windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,16 @@
 
 
 export CVXOPT_BLAS_LIB_DIR="${PREFIX}/lib"
-export CVXOPT_BLAS_LIB="openblas"
-export CVXOPT_LAPACK_LIB="openblas"
+
+if [ "$blas_impl" == "mkl" ]; then
+    export CVXOPT_BLAS_LIB="mkl_rt"
+    export CVXOPT_LAPACK_LIB="mkl_rt"
+elif [ "$blas_impl" == "openblas" ]; then
+    export CVXOPT_BLAS_LIB="openblas"
+    export CVXOPT_LAPACK_LIB="openblas"
+else
+    echo "blas_impl undefined in variant or not recognized.  Edit cvxopt's build.sh if you need to add a new supported blas"
+fi
 
 export CVXOPT_BUILD_GSL=1
 export CVXOPT_GSL_LIB_DIR="${PREFIX}/lib"

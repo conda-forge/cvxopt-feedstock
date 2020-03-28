d60084e28ac214f52c687ae616ae632b6d0c6fac
H. Vetinari <h.vetinari@gmx.com>
2020-03-28 13:40:13 +0100

    blas variants for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,27 @@
 
 
 set CVXOPT_MSVC=1
+:: the cvxopt builder for suitesparse looks for static ".lib" libraries (not ".dll"!)
 set "CVXOPT_BLAS_LIB_DIR=%LIBRARY_PREFIX%\lib"
 if "%blas_impl%" == "mkl" (
     set CVXOPT_BLAS_LIB=mkl_rt
     set CVXOPT_LAPACK_LIB=mkl_rt
 )
 if "%blas_impl%" == "openblas" (
+    :: see "openblas" output for windows here:
+    :: https://github.com/conda-forge/openblas-feedstock/blob/master/recipe/meta.yaml
     set CVXOPT_BLAS_LIB=openblas
-    set CVXOPT_LAPACK_LIB=openblas
+    :: see "blas" output for windows here:
+    :: https://github.com/conda-forge/lapack-feedstock/blob/master/recipe/meta.yaml
+    :: will be split on ';', see https://github.com/cvxopt/cvxopt/blob/master/setup.py
+    set CVXOPT_LAPACK_LIB=lapack
+)
+if "%blas_impl%" == "blis" (
+    :: see output for windows here:
+    :: https://github.com/conda-forge/blis-feedstock/blob/master/recipe/meta.yaml
+    set CVXOPT_BLAS_LIB=blis
+    :: same as above for openblas
+    set CVXOPT_LAPACK_LIB=lapack
 )
 
 set CVXOPT_BUILD_GSL=1
